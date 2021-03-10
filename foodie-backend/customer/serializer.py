5b46536c486e9bd76ce4175c08180493e1dfc8a3
unknown <fahimul.huq85@gmail.com>
2021-03-10 23:33:08 +0600

    customer signup system working

--- foodie-backend/customer/serializer.py
+++ foodie-backend/customer/serializer.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
+
+from user.serializer import UserSerializer
 from .models import Customer
 
 
 class CustomerSerializer(serializers.ModelSerializer):
-
-
-
+    user = UserSerializer(required=True)
 
     class Meta:
         model = Customer
-        fields = '__all__'
+        fields = ['user', 'profilePicture']
 
+    def create(self, validated_data):
+
+        user_data = validated_data.pop('user')
+        user = UserSerializer.create(UserSerializer(), validated_data=user_data)
+        customer, created = Customer.objects.update_or_create(user=user)
+        return customer
